67ecec4b1c18a9108ce8bd2a7415db85dd02d0d9
IamTenshi <angeljimenezabreu2@gmail.com>
2022-11-08 09:36:04 -0400

    I have to do the login and its validation

--- php/signin.php
+++ php/signin.php
@@ -1,5 +1,10 @@
 <?php 
+    //*Validate user
+    session_start();
+    require_once('config.php');
 
+    //TODO: Programar el inicio de sesion
+    //TODO: Validar el inicio de sesion
 
-
+    $conn->close();
 ?>
